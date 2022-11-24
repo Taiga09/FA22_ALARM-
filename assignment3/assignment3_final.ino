aba806fe36b3adb8704f7adc7ae3c9d82e152155
Taiga09 <118408939+Taiga09@users.noreply.github.com>
2022-11-23 17:37:18 -0800

    my code

--- assignment3/assignment3_final.ino
+++ assignment3/assignment3_final.ino
@@ -8,6 +8,16 @@ RTC_TimeTypeDef RTCtime, RTCTimeSave;
 RTC_TimeTypeDef AlarmTime;
 uint8_t second = 0, minutes = 0;
 
+//Colors
+#define LED_PIN    26
+#define LED_COUNT 18
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+const int GREEN = strip.Color(0,255,0);
+const int YELLOW = strip.Color(128,127,0,0);
+const int ORANGE = strip.Color(192,63,0,0);
+const int RED = strip.Color(255,0,0,0);
+int my_colors[] = {GREEN,YELLOW,ORANGE,RED,GREEN,YELLOW,ORANGE,RED,GREEN,YELLOW,ORANGE,RED};
+
 const int STATE_DEFAULT = 0;
 const int STATE_EDIT_HOURS = 1;
 const int STATE_EDIT_MINUTES = 2;
@@ -16,32 +26,42 @@ const int STATE_ALARM_FINISHED = 5;
 int program_state = STATE_DEFAULT;
 
 unsigned long rtcTimer = 0;
-
 unsigned long underlineTimer = 0;
 bool underlineOn = false;
 
 unsigned long ledBlinkTimer = 0;
 bool ledBlinkOn = false;
 
-const int sensorPin = 33;  // 4-wire bottom connector input pin used by M5 units
+const int sensorPin = 25;  // 4-wire bottom connector input pin used by M5 units
 int sensorVal = 0;
+int proximityVal = 0;
+int brightnessVal = 100;
 unsigned long sensorTimer = 0;
-int brightnessVal = 0;
+unsigned long beepTimer = 0;
+unsigned long ledTimer = 0;
 
-const int rgbledPin = 32; 
+int ledCounter = 0;
 
+//const int rgbledPin = 32; 
+/*
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(
     3, // number of LEDs
     rgbledPin, // pin number
     NEO_GRB + NEO_KHZ800);  // LED type
+*/
 
 void setup() {
-  M5.begin();
+  M5.begin(false,false,true);
+  strip.begin();
+  pinMode(sensorPin, INPUT);
+  //pinMode(rgbledPin, OUTPUT);
   Serial.begin(9600);
 
   M5.rtc.GetTime(&RTCTimeSave);
   AlarmTime = RTCTimeSave;
   AlarmTime.Minutes = AlarmTime.Minutes + 2;  // set alarm 2 minutes ahead 
+  beepTimer = millis();
+  ledTimer = millis();
   M5.update();
   
   M5.M5Ink.clear();
@@ -50,10 +70,11 @@ void setup() {
   checkRTC();
   PageSprite.creatSprite(0, 0, 200, 200);
   drawTime();
-  
+  /*
   pixels.begin();           // initialize NeoPixel strip object 
   pixels.show();            // turn OFF all pixels 
   pixels.setBrightness(50); // set brightness to about 1/5 (max = 255)
+  */
 }
 
 void loop() {
@@ -79,7 +100,13 @@ void loop() {
       Serial.println(input);
     }
   }
-  
+
+  //proximity sensor
+  sensorVal = analogRead(sensorPin);
+  proximityVal = map(sensorVal, 0, 4095, 0, 255);
+  Serial.print("proximityVal = ");
+  Serial.println(proximityVal);
+
   if( program_state == STATE_DEFAULT) {
     // state behavior: check and update time every second
     if(millis() > rtcTimer + 1000) {
@@ -88,6 +115,7 @@ void loop() {
       drawTimeToAlarm();      
       rtcTimer = millis();
     }
+    /*
     // state behavior: read sensor and print its value to Serial port
     if(millis() > sensorTimer + 100) {
       sensorVal = analogRead(sensorPin);
@@ -100,18 +128,23 @@ void loop() {
       pixels.setPixelColor(i, pixels.Color(0, brightnessVal, 0)); 
       pixels.show(); 
     }
+    */
+
+    // state transition: alarm time equals real time clock 
+    if(AlarmTime.Hours == RTCtime.Hours && AlarmTime.Minutes == RTCtime.Minutes) {
+      program_state = STATE_ALARM;
+      Serial.println("program_state => STATE_ALARM");
+    }
+    
     // state transition: MID button 
     if ( M5.BtnMID.wasPressed()) {
       AlarmTime = RTCtime;
       program_state = STATE_EDIT_MINUTES;
       Serial.println("program_state => STATE_EDIT_MINUTES");
     }
-    // state transition: alarm time equals real time clock 
-    else if(AlarmTime.Hours == RTCtime.Hours && AlarmTime.Minutes == RTCtime.Minutes) {
-      program_state = STATE_ALARM;
-      Serial.println("program_state => STATE_ALARM");
-    }
+   
   }
+    
   else if( program_state == STATE_EDIT_MINUTES) {
     // state behavior: blink underline under alarm minutes
     if( millis() > underlineTimer + 250 ) {
@@ -194,46 +227,97 @@ void loop() {
     if(millis() > rtcTimer + 1000) {
       updateTime();
       drawTime();
-      //drawTimeToAlarm();    
-      PageSprite.drawString(45, 130, "  ALARM IS ON  ");
-      PageSprite.pushSprite();   
+      drawTimeToAlarm();      
       rtcTimer = millis();
     }
-    // (OPTIONAL) state behavior: blink RGB LEDs red every 500ms
-    if(millis() > ledBlinkTimer + 500) {
-      if(ledBlinkOn) {
-        // turn all pixels red:
-        for( int i=0; i<3; i++) {
-          pixels.setPixelColor(i, pixels.Color(255, 0, 0)); 
-          pixels.show(); 
-        }
-        ledBlinkOn = false;        
-      }
-      else {
-        // turn all pixels off:
-        for( int i=0; i<3; i++) {
-          pixels.setPixelColor(i, pixels.Color(0, 0, 0)); 
-          pixels.show(); 
-        }
-        ledBlinkOn = true;
-      }
-      ledBlinkTimer = millis();
+
+    //beep sound 
+    if (millis()>beepTimer + 800){
+      M5.Speaker.setBeep(500,500);
+      M5.Speaker.beep();
+      beepTimer = millis();
     }
-    // state transition: top button press to finish alarm
-    if ( M5.BtnEXT.wasPressed()) {
-      Serial.println("BtnEXT wasPressed!");
-      M5.M5Ink.clear();
-      PageSprite.clear(CLEAR_DRAWBUFF | CLEAR_LASTBUFF);
-      program_state = STATE_ALARM_FINISHED;
-      Serial.println("program_state => STATE_ALARM_FINISHED");
+    
+    //strip led animation
+    if (millis() > ledTimer + 200) { //350) {
+        int myColorValue1 = my_colors[ledCounter];
+        int myColorValue2 = my_colors[ledCounter+1];
+        int myColorValue3 = my_colors[ledCounter+2];
+        strip.setPixelColor(0, myColorValue1);//pixels.Color(myColorValue));
+        strip.setPixelColor(1, myColorValue2);//pixels.Color(myColorValue));
+        strip.setPixelColor(2, myColorValue3);//pixels.Color(myColorValuel));
+        strip.setPixelColor(3, myColorValue1);//pixels.Color(myColorValuel));
+        strip.setPixelColor(4, myColorValue2);//pixels.Color(myColorValuel));
+        strip.setPixelColor(5, myColorValue3);//pixels.Color(myColorValuel));
+        strip.setPixelColor(7, myColorValue1);//pixels.Color(myColorValuel));
+        strip.setPixelColor(8, myColorValue2);//pixels.Color(myColorValuel));
+        strip.setPixelColor(9, myColorValue3);//pixels.Color(myColorValuel));
+        strip.setPixelColor(10, myColorValue1);//pixels.Color(myColorValuel));
+        strip.setPixelColor(11, myColorValue2);//pixels.Color(myColorValuel));
+        strip.setPixelColor(12, myColorValue3);//pixels.Color(myColorValuel));
+        strip.setPixelColor(13, myColorValue1);//pixels.Color(myColorValuel));
+        strip.setPixelColor(14, myColorValue2);//pixels.Color(myColorValuel));
+        strip.setPixelColor(15, myColorValue3);//pixels.Color(myColorValuel));
+        strip.setPixelColor(16, myColorValue1);//pixels.Color(myColorValuel));
+        strip.setPixelColor(17, myColorValue2);//pixels.Color(myColorValuel));
+        strip.setPixelColor(18, myColorValue3);//pixels.Color(myColorValuel));
+        strip.show();
+        
+        if(ledCounter < 11)
+           ledCounter++;
+         else
+           ledCounter = 0;
+      ledTimer = millis();
     }
+    
+    //proximity sensor
+    sensorVal = analogRead(sensorPin);
+    proximityVal = map(sensorVal, 0, 4095, 0, 255);
+    Serial.print("proximityVal = ");
+    Serial.println(proximityVal);
+    
+    // state transition: hold a hand for 7 secs to finish alarm
+    if(proximityVal > 120){
+      if (millis() > sensorTimer + 6000) {
+        Serial.println("Alarm is off!!");
+        M5.M5Ink.clear();
+        PageSprite.clear(CLEAR_DRAWBUFF | CLEAR_LASTBUFF);
+        program_state = STATE_ALARM_FINISHED;
+        Serial.println("program_state => STATE_ALARM_FINISHED");
+        
+        //turn of the light
+        strip.setPixelColor(0, 0);
+        strip.setPixelColor(1, 0);
+        strip.setPixelColor(2, 0);
+        strip.setPixelColor(3, 0);
+        strip.setPixelColor(4, 0);
+        strip.setPixelColor(5, 0);
+        strip.setPixelColor(7, 0);
+        strip.setPixelColor(8, 0);
+        strip.setPixelColor(9, 0);
+        strip.setPixelColor(10, 0);
+        strip.setPixelColor(11, 0);
+        strip.setPixelColor(12, 0);
+        strip.setPixelColor(13, 0);
+        strip.setPixelColor(14, 0);
+        strip.setPixelColor(15, 0);
+        strip.setPixelColor(16, 0);
+        strip.setPixelColor(17, 0);
+        strip.setPixelColor(18, 0);
+        strip.show();
+      }
+    }
+    
+    M5.update();
   }
+  
+  
   else if(program_state == STATE_ALARM_FINISHED) {
     // state behavior: check and update time every second
     if(millis() > rtcTimer + 1000) {
       updateTime();
       drawTime();
-      PageSprite.drawString(50, 130, "ALARM IS OFF");
+      PageSprite.drawString(50, 120, "ALARM IS OFF");
       PageSprite.pushSprite();    
       rtcTimer = millis();
     }
@@ -247,4 +331,3 @@ void loop() {
   
   M5.update();
 }
-
